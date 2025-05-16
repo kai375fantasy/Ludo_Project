632915e814097c7f4cadc8e79c05e13ec22869d2
Junkai Qian <67379696+kai375fantasy@users.noreply.github.com>
2025-05-16 13:47:47 +0100

    Delete Ci536_Group_Ludo directory

--- Ci536_Group_Ludo/js/main.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    type: Phaser.AUTO,
-    width: 1000,
-    height: 600,
-    backgroundColor: '#ffffff',
-    scene: [BootScene, MainMenuScene, PlayScene, GameOverScene],
-};
-
-const game = new Phaser.Game(config);
